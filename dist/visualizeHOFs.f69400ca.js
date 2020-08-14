03a3cd4dad4bec1862410309c2891324ac91c261
Paul Ferguson <paullferguson@gmail.com>
2020-08-14 16:18:03 -0700

    Tidy up ignores

--- dist/visualizeHOFs.f69400ca.js
+++ dist/visualizeHOFs.f69400ca.js
@@ -28525,7 +28525,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56525" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64830" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
